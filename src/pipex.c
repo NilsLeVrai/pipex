154c44dd7b1ebddd931a35a8812c3c063b6e86b6
Nils Abraham <nils@sargasso.home>
2024-08-19 22:23:12 +0200

    new without files

--- src/pipex.c
+++ src/pipex.c
@@ -3,66 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: niabraha <niabraha@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 23:28:00 by niabraha          #+#    #+#             */
-/*   Updated: 2024/06/18 19:02:47 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/08/19 22:22:54 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-void	first_child_process(t_pipex fd, char *cmd1, char **envp, char **argv)
+static void	first_child_process(t_pipex fd, char *cmd1, char **envp)
 {
-	fd.infile = open(argv[1], O_RDONLY);
-	if (fd.infile < 0)
-	{
-		close(fd.pipefd[0]);
-		close(fd.pipefd[1]);
-		error_message(argv[1]);
-	}
-	if (dup2(fd.infile, STDIN_FILENO) == -1)
-		error_message("dup2 infile");
 	if (dup2(fd.pipefd[1], STDOUT_FILENO) == -1)
 		error_message("dup2 pipefd[1]");
 	close(fd.pipefd[0]);
 	close(fd.pipefd[1]);
-	close(fd.infile);
 	ft_execlp(cmd1, envp);
 }
 
-void	second_child_process(t_pipex fd, char *cmd2, char **envp, char **argv)
+static void	second_child_process(t_pipex fd, char *cmd2, char **envp)
 {
-	fd.outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd.outfile < 0)
-	{
-		close(fd.pipefd[0]);
-		close(fd.pipefd[1]);
-		error_message(argv[4]);
-	}
-	if (dup2(fd.outfile, STDOUT_FILENO) == -1)
-		error_message("dup2 outfile");
 	if (dup2(fd.pipefd[0], STDIN_FILENO) == -1)
 		error_message("dup2 pipefd[0]");
 	close(fd.pipefd[0]);
 	close(fd.pipefd[1]);
-	close(fd.outfile);
 	ft_execlp(cmd2, envp);
 }
 
-void	parent_process(t_pipex *fd, pid_t *pid)
+static void	parent_process(t_pipex *fd)
 {
 	close(fd->pipefd[0]);
 	close(fd->pipefd[1]);
-	waitpid(pid[0], &(fd->status), 0);
-	waitpid(pid[1], &(fd->status), 0);
+	waitpid(fd->pid[0], &(fd->status), 0);// le goat
+	waitpid(fd->pid[1], &(fd->status), 0);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	t_pipex	fd;
 
-	if (argc != 5)
+	if (argc != 3)
 		return (ft_printf("Wrong number of arguments\n"));
 	if (pipe(fd.pipefd) == -1)
 		return (error_message("Pipe error"), -1);
@@ -70,16 +50,16 @@ int	main(int argc, char **argv, char **envp)
 	if (fd.pid[0] < 0)
 		return (error_message("Fork error on pid[0]"), -1);
 	if (fd.pid[0] == 0)
-		first_child_process(fd, argv[2], envp, argv);
+		first_child_process(fd, argv[1], envp);
 	else
 	{
 		fd.pid[1] = fork();
 		if (fd.pid[1] < 0)
 			return (error_message("Fork error on pid[1]"), -1);
 		if (fd.pid[1] == 0)
-			second_child_process(fd, argv[3], envp, argv);
+			second_child_process(fd, argv[2], envp);
 		else
-			parent_process(&fd, fd.pid);
+			parent_process(&fd);
 	}
 	if (WIFEXITED(fd.status))
 		return (WEXITSTATUS(fd.status));
